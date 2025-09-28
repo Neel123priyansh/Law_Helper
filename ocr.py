44ba0ccd5692b7af73ac020ec4caf144f56dea78
Neel123priyansh <neelpriyansh@gmail.com>
2025-09-28 17:56:27 +0530

    OCR docTR

--- ocr.py
+++ ocr.py
@@ -1,58 +1,47 @@
-import cv2
 import pytesseract
+from pdf2image import convert_from_path
+import os
+import cv2
 import numpy as np
+from doctr.models import ocr_predictor
 
-# Path to tesseract (for Windows)
+#Model
+
+# Path to your scanned PDF
+pdf_path = "Test_PDF_For_OCR/D-303-Abode-Valley-Contract-May2025-Signed.pdf"
+output_txt = "agreement_text.txt"
+poppler_path = r"C:\Program Files\poppler-25.07.0\Library\bin" 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-def ocrcore(img):
-    config = r'--oem 3 --psm 6'
-    text = pytesseract.image_to_string(img, lang="eng", config=config)
-    return text
-
-# --- Preprocessing Functions ---
-def get_greyscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-def remove_noise(image):
-    return cv2.medianBlur(image, 3) 
-
-def thresholding(image):
-
-    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                 cv2.THRESH_BINARY, 31, 2)
-
-def deskew(image):
-    coords = cv2.findNonZero(cv2.bitwise_not(image))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = image.shape[:2]
-    M = cv2.getRotationMatrix2D((w // 2, h // 2), angle, 1.0)
-    return cv2.warpAffine(image, M, (w, h),
-                          flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-
-def upscale(image):
-    return cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-
-def morph_open_close(image):
-    kernel = np.ones((2,2), np.uint8)
-    opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-    return closing
-
-# --- Main Execution ---
-img = cv2.imread("ocr_ss.png")
-
-img = get_greyscale(img)
-img = upscale(img)
-img = thresholding(img)
-img = remove_noise(img)
-img = deskew(img)
-img = morph_open_close(img)
 
 
-text = ocrcore(img)
-print(text)
+# Step 1: Convert PDF pages to images
+pages = convert_from_path(pdf_path, dpi=300, poppler_path=poppler_path)
+
+#Step 2: Image Preprocessing
+def preprocess_image(pil_image):
+    # Convert PIL -> OpenCV
+    img = np.array(pil_image)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Remove background noise
+    _, img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+
+    # Optional: remove small dots
+    kernel = np.ones((1,1), np.uint8)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+    return img
+
+# Step 3: OCR each page
+full_text = ""
+for i, page in enumerate(pages):
+    clean_pages = preprocess_image(pages)
+    text = pytesseract.image_to_string(clean_pages, lang="eng") 
+    full_text += f"\n--- Page {i+1} ---\n{text}\n"
+
+# Step 4: Save to .txt file
+with open(output_txt, "w", encoding="utf-8") as f:
+    f.write(full_text)
+
+print(f"✅ OCR completed. Text saved to {output_txt}")
