a43d83d588c40c6a7420dced28d018ed15d10ea0
Neel123priyansh <neelpriyansh@gmail.com>
2025-09-30 23:55:55 +0530

    Lawyer OCR

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import streamlit as st
 from typing import List
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings 
 from langchain_google_genai import ChatGoogleGenerativeAI  
 from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS 
